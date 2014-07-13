f04c65a947595ee35f7a35bb975b7dd321737239
Hai Phan Nguyen <pnghai@gmail.com>
2014-07-13 17:24:11 +0700

    add php5.3 test

--- src/CEMS/Guzzle3.php
+++ src/CEMS/Guzzle3.php
@@ -7,7 +7,7 @@
  */
 
 namespace CEMS;
-use Guzzle\Client as GuzzleClient;
+use Guzzle\Http\Client as GuzzleClient;
 use Guzzle\Http\Exception as GuzzleException;
 
 class Guzzle3 implements GuzzleStrategy{
@@ -28,5 +28,31 @@ class Guzzle3 implements GuzzleStrategy{
     public function request($httpMethod='GET', $path='',  array $params = null, $version = null,$isAuthorization=false)
     {
         //TODO: Implement Guzzle 3 here
+        $guzzleClient=new GuzzleClient();
+        switch ($httpMethod) {
+            case 'GET':
+                $request = $guzzleClient->createRequest($httpMethod, $path,array(),array('query'=>$params));
+                break;
+            default:
+                $request = $guzzleClient->createRequest($httpMethod, $path, array(),array('body' => $params));
+        }
+        try {
+            $res = $request->send();
+        } catch (GuzzleException\ClientErrorResponseException $e) {
+            //catch error 404
+            $error_message=$e->getResponse();
+            if ($isAuthorization)
+                throw new AuthorizeException($error_message, $error_message->getCode(),$e->getPrevious());
+            else
+                throw new ClientException($error_message, $e->getResponse()->getStatusCode(),$e->getPrevious());
+        }
+        catch (GuzzleException\ServerErrorResponseException $e){
+            throw new ServerException($e, $e->getResponse()->getStatusCode(),$e->getPrevious());
+        }
+        catch (GuzzleException\BadResponseException $e) {
+            throw new Error($e->getResponse(), $e->getResponse()->getStatusCode(),$e->getPrevious());
+        }
+        $response = new Response($res->json());
+        return $response;
     }
 } 
